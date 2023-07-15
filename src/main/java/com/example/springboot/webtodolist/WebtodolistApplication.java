801dc60ad20164fb7ce043abe85958f2ec9894ad
ParkJoonha <parkaso1@gmail.com>
2023-07-15 20:43:18 +0900

    First Commit

---
+++ src/main/java/com/example/springboot/webtodolist/WebtodolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.webtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/webtodolist/WebtodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.webtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
